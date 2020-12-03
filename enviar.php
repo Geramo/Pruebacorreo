847c8497b08ddb23f4c4c288c1754e245ebdab6d
Geramo <62858575+Geramo@users.noreply.github.com>
2020-12-02 22:46:25 -0600

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,8 @@
+<?php
+$destino= "gerardo.r.design@gmail.com"; 
+$nombre = $_POST["nombre"];
+$correo = $_POST["correo"];
+$mensaje = $_POST["mensaje"];
+$contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo  . "\nMensaje: " . $mensaje; mail($destino,"Contacto", $contenido); 
+header ("Location:gracias.html"); 
+?>
